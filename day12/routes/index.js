ea1d2459692b142ace2e2582d0c59c6c101aab51
Yashvi <yashvigarg330@gmail.com>
2021-09-24 01:56:48 +0530

    done

---
+++ day12/routes/index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var registerInitialCheck = require("../middlewares/registerChecks");
+var register = require("../controllers/register");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/**
+ * @requires {email, password, confirmPassword} - req.body
+ * @description
+ * Security, performance and edge cases
+ * level - 01
+ * email validate - @.
+ * password validate
+ * level - 02
+ * JS / SQL
+ * level - 03
+ * check if email already ecists
+ * password hash
+ * email lowercase
+ * save
+ */
+
+router.post("/register", registerInitialCheck, register);
+
+module.exports = router;
