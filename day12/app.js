15ed1506ab7cc651ae2c35ca487384360aa7f309
Yashvi <yashvigarg330@gmail.com>
2021-09-24 22:12:12 +0530

    done

--- day12/app.js
+++ day12/app.js
@@ -3,6 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const { redisClient, RedisStore, session } = require("./database/redis");
+
+require("./database/mongo");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -18,6 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+//add redis middleware
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "secret$123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 1000 * 60 * 10,
+    },
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- day12/routes/index.js
+++ day12/routes/index.js
@@ -5,6 +5,13 @@ var register = require("../controllers/register");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  const session = req.session;
+  session.username = "bivas";
+  res.render("index", { title: "Express" });
+});
+
+router.get("/test", function (req, res, next) {
+  console.log("Redis Value", req.session.username);
   res.render("index", { title: "Express" });
 });
 
