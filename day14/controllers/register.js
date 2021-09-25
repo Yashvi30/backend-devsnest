2d75251913bfa69cc9e58b3fc4824838f3c1547f
Yashvi <yashvigarg330@gmail.com>
2021-09-25 22:34:53 +0530

    done

---
+++ day14/controllers/register.js
@@ -0,0 +1,38 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * check if email already ecists
+ * password hash
+ * email lowercase
+ * save
+ */
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const alreadyExists = await User.findOne({ where: { email } });
+    if (alreadyExists) {
+      res.status(401).send("Email already exists");
+    } else {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(password, salt);
+      const newUser = new User({
+        email: email.toLowerCase(),
+        password: hash,
+        fullName: fullName,
+      });
+      const savedUser = await newUser.save();
+      res.status(201).send(savedUser);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+};
+
+module.exports = register;
